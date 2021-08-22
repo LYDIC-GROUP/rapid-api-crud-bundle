da33c753b36f4e218a174614607e629e64ae994d
Willem Turkstra <willemturkstra@gmail.com>
2021-08-22 21:54:54 +0200

    #14 fix update entity issue

--- src/Event/AfterEntityUpdatedEvent.php
+++ src/Event/AfterEntityUpdatedEvent.php
@@ -7,7 +7,27 @@
  */
 namespace LydicGroup\RapidApiCrudBundle\Event;
 
-class AfterEntityUpdatedEvent extends BeforeEntityUpdatedEvent
+use LydicGroup\RapidApiCrudBundle\Context\RapidApiContext;
+use LydicGroup\RapidApiCrudBundle\Entity\RapidApiCrudEntity;
+
+class AfterEntityUpdatedEvent extends AbstractEntityEvent
 {
     public const NAME = 'entity.after.update';
+
+    private RapidApiCrudEntity $oldEntity;
+
+    public function __construct(RapidApiContext $context, RapidApiCrudEntity $entity, RapidApiCrudEntity $oldEntity)
+    {
+        parent::__construct($context, $entity);
+
+        $this->oldEntity = $oldEntity;
+    }
+
+    /**
+     * @return RapidApiCrudEntity
+     */
+    public function getOldEntity(): RapidApiCrudEntity
+    {
+        return $this->oldEntity;
+    }
 }
